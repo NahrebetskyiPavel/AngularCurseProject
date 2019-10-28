16680e4b8be77357dd6e18f86a2ae7c820c6f3be
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2019-10-28 22:18:53 +0200

    5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganazierComponent } from './organazier/organazier.component';
+import { MomentPipe } from './shared/moment.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganazierComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/organazier/organazier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organazier',
+  templateUrl: './organazier.component.html',
+  styleUrls: ['./organazier.component.scss']
+})
+export class OrganazierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

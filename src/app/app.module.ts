5287f2373ea873b30229486874e31a80ee1cdb73
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2019-11-08 21:50:27 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,16 @@ import { OrganazierComponent } from './organazier/organazier.component';
 import { MomentPipe } from './shared/moment.pipe';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AuthComponent } from './auth/auth/auth.component';
+import {RouterModule, Routes} from '@angular/router'
 
+const routes: Routes = [{
+  path: 'login', component: AuthComponent
+}]
 
 @NgModule({
   declarations: [
@@ -16,13 +25,18 @@ import { HttpClientModule } from '@angular/common/http';
     CalendarComponent,
     SelectorComponent,
     OrganazierComponent,
-    MomentPipe
+    MomentPipe,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
 
   ],
   providers: [],

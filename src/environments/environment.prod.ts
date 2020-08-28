1e73949c22e9930a336e1db6d64c055e0c1ed961
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2020-08-28 11:39:10 +0300

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig :{
+    apiKey: "AIzaSyDoZ6b2mvG_q6kusywO2vR_aMhTYsJCCac",
+    authDomain: "angular-project-bf2e0.firebaseapp.com",
+    databaseURL: "https://angular-project-bf2e0.firebaseio.com",
+    projectId: "angular-project-bf2e0",
+    storageBucket: "angular-project-bf2e0.appspot.com",
+    messagingSenderId: "854710681046",
+    appId: "1:854710681046:web:433729fefb271ae03fb877",
+    measurementId: "G-D6ZS59QYQT"
+  }
 };
